67223b4db2b4fb03fee32fedf508d719fdc6157e
Yogesh Dawkhar <yogesh.dawkhar@gmail.com>
2019-04-04 12:46:26 +0530

    Update screenshotutil.java
    previous code was not working on linux

--- src/test/java/com/deposit/yogeshdawkhar/util/ScreenshotUtil.java
+++ src/test/java/com/deposit/yogeshdawkhar/util/ScreenshotUtil.java
@@ -1,10 +1,11 @@
 package com.deposit.yogeshdawkhar.util;
 
 import java.io.File;
-import java.text.DateFormat;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -12,18 +13,15 @@ import com.deposit.yogeshdawkhar.helper.HelperClass;
 
 public class ScreenshotUtil extends HelperClass {
 
-	public static boolean takeScreenshot(final String name) {
-		String screenshotDirectory = EXTENT_REPORT_IMAGE_PATH;
+	public static void takeScreenshot(final String name) throws IOException {
+		String screenshotDirectory = extentReportImagePath;
 		File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		return screenshot.renameTo(new File(screenshotDirectory, String.format("%s.png", name)));
+		FileUtils.copyFile(screenshot, (new File(screenshotDirectory, String.format("%s.png", name))));
 	}
 
-	public static String createimagename() throws Exception {
-		DateFormat dateFormat = new SimpleDateFormat("yyMMddHHmmssSSS");
-		Date date = new Date();
-		String imagename = dateFormat.format(date);
+	public static String createimagename() throws IOException {
+		String imagename = new SimpleDateFormat("dd_MMM_yy_HH_mm_ss_SSS").format(new Date());
 		takeScreenshot(imagename);
 		return imagename;
 	}
-
 }
